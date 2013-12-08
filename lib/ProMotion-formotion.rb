68a3b56e01ce2bbb9fbda5f2f565692e190887f2
Justin McNally <justin@kohactive.com>
2013-12-08 00:09:45 -0600

    motion require and updated docs

--- lib/ProMotion-formotion.rb
+++ lib/ProMotion-formotion.rb
@@ -2,8 +2,8 @@ unless defined?(Motion::Project::Config)
   raise "This file must be required within a RubyMotion project Rakefile."
 end
 
+require "formotion"
+require "motion-require"
 require "ProMotion/formotion/version"
 
-Motion::Project::App.setup do |app|
-  app.files = Dir.glob(File.join(File.dirname(__FILE__), 'ProMotion/**/*.rb')) | app.files
-end
+Motion::Require.all(Dir.glob(File.expand_path('../ProMotion/**/*.rb', __FILE__)))
