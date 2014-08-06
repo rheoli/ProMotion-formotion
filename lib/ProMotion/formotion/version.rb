10510b81c590014b0289c243e452a2bd5f98b193
Stephan Toggweiler <stephan.toggweiler@hispeed.ch>
2014-08-06 21:28:23 +0200

    version 0.1.5

--- lib/ProMotion/formotion/version.rb
+++ lib/ProMotion/formotion/version.rb
@@ -1,5 +1,5 @@
 module ProMotion
   module Formotion
-    VERSION = "0.1.4" unless defined?(ProMotion::Formotion::VERSION)
+    VERSION = "0.1.5" unless defined?(ProMotion::Formotion::VERSION)
   end
 end
